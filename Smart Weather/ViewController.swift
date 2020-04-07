a52ca62bcf155c3d5dd57e40fac3c2fb8db6a5e8
Digi Developer <jassim@digipresence.net>
2020-04-07 09:24:46 +0400

    Initial Commit

---
+++ Smart Weather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Smart Weather
+//
+//  Created by Digi Developer on 07/04/2020.
+//  Copyright © 2020 Jassim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
